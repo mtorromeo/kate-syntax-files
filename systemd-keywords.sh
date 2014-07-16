a52b1a6f9183ba1d92d20cffccfaa082838ad3f1
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2014-07-16 12:11:38 +0200

    Implemented section names extraction from the man pages for systemd

--- systemd-keywords.sh
+++ systemd-keywords.sh
@@ -1,3 +1,15 @@
-#!/bin/sh
-keywords=$(zgrep -Poh '(?<=\\fI)[A-Z][A-Za-z0-9]+(?==\\fR)' /usr/share/man/man5/systemd.*.5.gz | sort | uniq | awk '{printf "\\t\\t<item>"$1"</item>\\n"}')
-perl -0777pe 's#<list name="keywords">.*?</list>#<list name="keywords">\n'$keywords'\t</list>#s' -i systemd.xml
+#!/bin/bash
+GREP="zgrep -Poh"
+MANPAGES="/usr/share/man/man5/systemd.*.5.gz"
+
+replacelist() {
+	name=$1
+	items=$(echo "$2" | sort | uniq -i | awk '{printf "\\t\\t<item>"$1"</item>\\n"}')
+	perl -0777pe 's#<list name="'$name'">.*?</list>#<list name="'$name'">\n'$items'\t</list>#s' -i systemd.xml
+}
+
+items=$($GREP '(?<=\\fI)[A-Z][A-Za-z0-9]+(?==\\fR)' $MANPAGES)
+replacelist keywords "$items"
+
+items=$($GREP '(?<="\[)[A-Za-z]+(?=\]")' $MANPAGES; $GREP -i '(?<=\[)[A-Za-z]+(?=\]"? section)' $MANPAGES)
+replacelist sections "$items"
